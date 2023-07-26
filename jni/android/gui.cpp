1d9a59745d6dc3b567086ae878afe2ca7a0a7e80
Dayvid Albuquerque <115833146+D4yvid@users.noreply.github.com>
2023-07-26 14:51:19 -0300

    Forgot to change the CPP file :skull:

--- jni/android/gui.cpp
+++ jni/android/gui.cpp
@@ -6,5 +6,5 @@ sp<Surface> SurfaceControl::getSurface() const { return sp<Surface>(); }
 
 SurfaceComposerClient::SurfaceComposerClient() {}
 SurfaceComposerClient::~SurfaceComposerClient() {}
-sp<IBinder> SurfaceComposerClient::getBuiltInDisplay(int32_t id) { return sp<IBinder>(); }
+sp<IBinder> SurfaceComposerClient::getInternalDisplayToken() { return sp<IBinder>(); }
 status_t SurfaceComposerClient::getDisplayInfo(const sp<IBinder> & display, DisplayInfo * info) { return 0; }
